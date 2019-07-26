00e4e4e095e9b130a11405e9b092c1f35431c401
Tetsuya Mori <monry84@gmail.com>
2019-07-26 13:57:47 +0900

    :white_check_mark: Fix test for CameraEvent
    * Use Component.enabled instead of controlling position

--- Assets/Tests/PlayMode/Scripts/CameraEventTest.cs
+++ Assets/Tests/PlayMode/Scripts/CameraEventTest.cs
@@ -28,7 +28,7 @@ namespace EventConnector
                             .First(x => x.name == "CubeWillVisible");
                         Assert.NotNull(go);
                         go.SetActive(true);
-                        Observable.Timer(TimeSpan.FromSeconds(0.1)).Subscribe(_ => go.transform.position = Vector3.zero);
+                        Observable.Timer(TimeSpan.FromSeconds(0.1)).Subscribe(_ => go.GetComponent<MeshRenderer>().enabled = true);
                     },
                     0.5
                 );
@@ -49,7 +49,7 @@ namespace EventConnector
                             .First(x => x.name == "CubeWillInvisible");
                         Assert.NotNull(go);
                         go.SetActive(true);
-                        Observable.Timer(TimeSpan.FromSeconds(0.1)).Subscribe(_ => go.transform.position = new Vector3(0.0f, 2.0f, 0.0f));
+                        Observable.Timer(TimeSpan.FromSeconds(0.1)).Subscribe(_ => go.GetComponent<MeshRenderer>().enabled = false);
                     },
                     0.5
                 );
@@ -57,20 +57,30 @@ namespace EventConnector
 
         private void AssertCameraBecomeVisibleEvent(EventMessages eventMessages)
         {
+            Assert.NotNull(eventMessages);
             Assert.AreEqual(1, eventMessages.Count);
 
             Assert.IsInstanceOf<CameraEvent>(eventMessages[0].sender);
             Assert.IsInstanceOf<CameraEventData>(eventMessages[0].eventData);
+
+            Assert.NotNull(eventMessages[0].eventData);
+
+            Assert.AreEqual(CameraEventType.BecomeVisible, ((CameraEventData) eventMessages[0].eventData).EventType);
 
             HasAssert = true;
         }
 
         private void AssertCameraBecomeInvisibleEvent(EventMessages eventMessages)
         {
+            Assert.NotNull(eventMessages);
             Assert.AreEqual(1, eventMessages.Count);
 
             Assert.IsInstanceOf<CameraEvent>(eventMessages[0].sender);
             Assert.IsInstanceOf<CameraEventData>(eventMessages[0].eventData);
+
+            Assert.NotNull(eventMessages[0].eventData);
+
+            Assert.AreEqual(CameraEventType.BecomeInvisible, ((CameraEventData) eventMessages[0].eventData).EventType);
 
             HasAssert = true;
         }
