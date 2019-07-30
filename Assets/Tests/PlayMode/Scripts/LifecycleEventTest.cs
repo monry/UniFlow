d2b854d61be32a9e947050aaa20ac00a9331c3fc
Tetsuya Mori <monry84@gmail.com>
2019-07-30 19:08:23 +0900

    :white_check_mark: Write test about Lifecycle

---
+++ Assets/Tests/PlayMode/Scripts/LifecycleEventTest.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections;
+using System.Linq;
+using EventConnector.Connector;
+using EventConnector.Message;
+using NUnit.Framework;
+using UniRx;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using Object = UnityEngine.Object;
+
+namespace EventConnector
+{
+    public class LifecycleEventTest : EventConnectorTestBase
+    {
+        private LifecycleEventType CurrentLifecycleEventType { get; set; }
+
+        [UnityTest]
+        public IEnumerator LifecycleStartEvent()
+        {
+            yield return RunAssertInternal(
+                LifecycleEventType.Start
+            );
+        }
+
+        [UnityTest]
+        public IEnumerator LifecycleUpdateEvent()
+        {
+            yield return RunAssertInternal(
+                LifecycleEventType.Update
+            );
+        }
+
+        [UnityTest]
+        public IEnumerator LifecycleFixedUpdateEvent()
+        {
+            yield return RunAssertInternal(
+                LifecycleEventType.FixedUpdate
+            );
+        }
+
+        [UnityTest]
+        public IEnumerator LifecycleLateUpdateEvent()
+        {
+            yield return RunAssertInternal(
+                LifecycleEventType.LateUpdate
+            );
+        }
+
+        [UnityTest]
+        public IEnumerator LifecycleDestroyEvent()
+        {
+            yield return RunAssertInternal(
+                LifecycleEventType.Destroy,
+                go => Observable.Timer(TimeSpan.FromSeconds(0.1f)).Subscribe(_ => Object.Destroy(go))
+            );
+        }
+
+        [UnityTest]
+        public IEnumerator LifecycleEnableEvent()
+        {
+            yield return RunAssertInternal(
+                LifecycleEventType.Enable
+            );
+        }
+
+        [UnityTest]
+        public IEnumerator LifecycleDisableEvent()
+        {
+            yield return RunAssertInternal(
+                LifecycleEventType.Disable,
+                go => Observable.Timer(TimeSpan.FromSeconds(0.1f)).Subscribe(_ => go.SetActive(false))
+            );
+        }
+
+        private IEnumerator RunAssertInternal(LifecycleEventType lifecycleEventType, Action<GameObject> callback = null)
+        {
+            yield return
+                RunAssert(
+                    "LifecycleEvent",
+                    AssertLifecycleEvent,
+                    () =>
+                    {
+                        CurrentLifecycleEventType = lifecycleEventType;
+                        var go = SceneManager
+                            .GetSceneByName("LifecycleEvent")
+                            .GetRootGameObjects()
+                            .First(x => x.name == lifecycleEventType.ToString());
+                        Assert.NotNull(go);
+                        go.SetActive(true);
+                        callback?.Invoke(go);
+                    },
+                    0.5
+                );
+        }
+
+        private void AssertLifecycleEvent(EventMessages eventMessages)
+        {
+            Assert.NotNull(eventMessages);
+            Assert.GreaterOrEqual(eventMessages.Count, 1);
+
+            Assert.IsInstanceOf<LifecycleEvent>(eventMessages[0].sender);
+            Assert.IsInstanceOf<LifecycleEventData>(eventMessages[0].eventData);
+
+            Assert.AreEqual(CurrentLifecycleEventType, ((LifecycleEventData) eventMessages[0].eventData).EventType);
+
+            HasAssert = true;
+        }
+    }
+}
