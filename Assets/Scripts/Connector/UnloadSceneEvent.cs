50671ee4774d61bc25d28a3633f86fe4371c01fb
Tetsuya Mori <monry84@gmail.com>
2019-08-22 21:17:27 +0900

    :sparkles: Support Load/Unload scenes using enum

--- Assets/Scripts/Connector/UnloadSceneEvent.cs
+++ Assets/Scripts/Connector/UnloadSceneEvent.cs
@@ -1,24 +1,20 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using JetBrains.Annotations;
 using UniRx;
 using UniRx.Async;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Zenject;
 
 namespace UniFlow.Connector
 {
-    [AddComponentMenu("UniFlow/UnloadScene", (int) ConnectorType.UnloadScene)]
-    public class UnloadSceneEvent : ConnectorBase
+    public abstract class UnloadSceneEventBase : ConnectorBase
     {
-        [SerializeField] private List<string> sceneNames = default;
-
-        [UsedImplicitly] public IEnumerable<string> SceneNames
-        {
-            get => sceneNames;
-            set => sceneNames = value.ToList();
-        }
+        [UsedImplicitly]
+        public abstract IEnumerable<string> SceneNames { get; set; }
 
         public override IObservable<EventMessage> OnConnectAsObservable()
         {
@@ -33,6 +29,39 @@ namespace UniFlow.Connector
             {
                 await SceneManager.UnloadSceneAsync(sceneName);
             }
+        }
+    }
+
+    [AddComponentMenu("UniFlow/UnloadScene", (int) ConnectorType.UnloadScene)]
+    public class UnloadSceneEvent : UnloadSceneEventBase
+    {
+        [SerializeField] private List<string> sceneNames = default;
+
+        [UsedImplicitly] public override IEnumerable<string> SceneNames
+        {
+            get => sceneNames;
+            set => sceneNames = value.ToList();
+        }
+    }
+
+    public abstract class UnloadSceneEvent<TSceneName> : UnloadSceneEventBase where TSceneName : Enum
+    {
+        [SerializeField] private List<TSceneName> sceneNames = default;
+
+        [InjectOptional(Id = InjectId.SceneNamePrefix)] private string SceneNamePrefix { get; }
+
+        [UsedImplicitly] public override IEnumerable<string> SceneNames
+        {
+            get => sceneNames.Select(x => $"{SceneNamePrefix}{x.ToString()}");
+            set => sceneNames = value
+                .Select(
+                    x => (TSceneName) Enum
+                        .Parse(
+                            typeof(TSceneName),
+                            Regex.Replace(x, $"^{SceneNamePrefix}", string.Empty)
+                        )
+                )
+                .ToList();
         }
     }
 }
