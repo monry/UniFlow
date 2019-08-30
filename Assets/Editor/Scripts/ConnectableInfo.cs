e92895af33bfb9775877e023bd36ddb4628de591
Tetsuya Mori <monry84@gmail.com>
2019-08-30 14:34:24 +0900

    :sparkles: Implement Undo

--- Assets/Editor/Scripts/ConnectableInfo.cs
+++ Assets/Editor/Scripts/ConnectableInfo.cs
@@ -2,25 +2,80 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using JetBrains.Annotations;
 using UnityEngine;
 
 namespace UniFlow.Editor
 {
+    [Serializable]
     public class ConnectableInfo
     {
-        private ConnectableInfo(GameObject gameObject, Type type, string name, IEnumerable<Parameter> parameters)
+        private ConnectableInfo(GameObject gameObject, IConnectable connectable, Type type, string name, IEnumerable<Parameter> parameters)
         {
             GameObject = gameObject;
+            Connectable = connectable;
             Type = type;
             Name = name;
-            Parameters = parameters;
+            parameterList = parameters.ToList();
         }
 
         public GameObject GameObject { get; set; }
+        public IConnectable Connectable { get; set; }
         public Type Type { get; }
         public string Name { get; }
-        public IEnumerable<Parameter> Parameters { get; }
+        [SerializeField] private List<Parameter> parameterList = default;
+        public IEnumerable<Parameter> ParameterList => parameterList;
 
+        public void ApplyParameter(Parameter parameter)
+        {
+            if (Connectable == default)
+            {
+                return;
+            }
+
+            Type
+                .GetField(parameter.Name, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)?
+                .SetValue(Connectable, parameter.Value);
+        }
+
+        public void ApplyParameters()
+        {
+            if (Connectable == default)
+            {
+                return;
+            }
+
+            ParameterList.ToList().ForEach(ApplyParameter);
+        }
+
+        [PublicAPI]
+        public static ConnectableInfo Create(IConnectable instance)
+        {
+            return Create(instance.GetType(), instance);
+        }
+
+        [PublicAPI]
+        public static ConnectableInfo Create(Type type, IConnectable instance = default)
+        {
+            return Create(
+                ((Component) instance)?.gameObject,
+                instance,
+                type,
+                type.Name,
+                type
+                    .GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                    .Where(x => x.GetCustomAttribute<SerializeField>() != null || x.IsPublic)
+                    .Select(x => Parameter.Create(x.FieldType, x.Name, instance != default ? x.GetValue(instance) : default))
+            );
+        }
+
+        [PublicAPI]
+        public static ConnectableInfo Create(GameObject gameObject, IConnectable instance, Type type, string name, IEnumerable<Parameter> parameters)
+        {
+            return new ConnectableInfo(gameObject, instance, type, name, parameters);
+        }
+
+        [Serializable]
         public class Parameter
         {
             private Parameter(Type type, string name, object value)
@@ -30,38 +85,20 @@ namespace UniFlow.Editor
                 Value = value;
             }
 
+            [SerializeField] private object value = default;
+
             public Type Type { get; }
             public string Name { get; }
-            public object Value { get; set; }
+            public object Value
+            {
+                get => value;
+                set => this.value = value;
+            }
 
             public static Parameter Create(Type type, string name, object value)
             {
                 return new Parameter(type, name, value);
             }
-        }
-
-        public static ConnectableInfo Create(IConnectable instance)
-        {
-            return Create(instance.GetType(), instance);
-        }
-
-        public static ConnectableInfo Create(Type type, IConnectable instance = default)
-        {
-            return
-                new ConnectableInfo(
-                    ((Component) instance)?.gameObject,
-                    type,
-                    type.Name,
-                    type
-                        .GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
-                        .Where(x => x.GetCustomAttribute<SerializeField>() != null || x.IsPublic)
-                        .Select(x => Parameter.Create(x.FieldType, x.Name, instance != default ? x.GetValue(instance) : default))
-                );
-        }
-
-        public static ConnectableInfo Create(GameObject gameObject, Type type, string name, IEnumerable<Parameter> parameters)
-        {
-            return new ConnectableInfo(gameObject, type, name, parameters);
         }
     }
 }
