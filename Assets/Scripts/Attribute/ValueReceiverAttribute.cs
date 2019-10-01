59b1c44937eb19a159acd42bac819ab593c37616
Tetsuya Mori <monry84@gmail.com>
2019-10-01 14:06:28 +0900

    :wrench: Avoid warning on Rider

--- Assets/Scripts/Attribute/ValueReceiverAttribute.cs
+++ Assets/Scripts/Attribute/ValueReceiverAttribute.cs
@@ -1,8 +1,10 @@
 using System;
+using JetBrains.Annotations;
 
 namespace UniFlow.Attribute
 {
     [AttributeUsage(AttributeTargets.Property)]
+    [MeansImplicitUse]
     public class ValueReceiverAttribute : System.Attribute
     {
         public ValueReceiverAttribute(string name = "")
