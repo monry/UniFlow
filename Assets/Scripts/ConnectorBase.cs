d4c243d6b277f75a49ac0055a846770d1e82b883
Tetsuya Mori <monry84@gmail.com>
2019-10-07 21:47:10 +0900

    :beetle: Make sure to publish to multiple target connectors

--- Assets/Scripts/ConnectorBase.cs
+++ Assets/Scripts/ConnectorBase.cs
@@ -89,7 +89,7 @@ namespace UniFlow
                 );
             if (TargetConnectors.Count() > 1)
             {
-                observable = observable.Share();
+                observable = observable.Replay().RefCount();
             }
 #if UNITY_EDITOR
             TargetConnectors
