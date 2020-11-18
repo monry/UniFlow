3b11894a19a70728c89e268e8fd147d12e2d2ab7
Shu Masuda <masushu@gmail.com>
2020-11-18 17:41:06 +0900

    :wrench: RuntimePlatformProvider didn't work

--- Assets/Scripts/Connector/ValueProvider/RuntimePlatformProvider.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace UniFlow.Connector.ValueProvider
-{
-    [AddComponentMenu("UniFlow/ValueProvider/RuntimePlatformProvider", (int) ConnectorType.ValueProviderRuntimePlatform)]
-    public class RuntimePlatformProvider : ProviderBase<RuntimePlatform>
-    {
-    }
-}
